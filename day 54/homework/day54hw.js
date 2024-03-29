e3ab94e96e8269d7bfabcf4d573f2bc56bbe3ccd
tatulibozhadze <144163682+tatulibozhadze@users.noreply.github.com>
2024-03-29 06:30:09 -0700

    Add files via upload

---
+++ day 54/homework/day54hw.js
@@ -0,0 +1,11 @@
+let myString = "42";
+
+let myInteger = 42;
+
+let stringToInteger = Number(myString);
+let integerToString = String(myInteger);
+
+console.log("String:", myString); 
+console.log("Integer:", myInteger); 
+console.log("String to Integer:", stringToInteger); 
+console.log("Integer to String:", integerToString); 
---
+++ day 54/homework/day54hw2.js
@@ -0,0 +1,9 @@
+let name = "John";
+
+let age = 30;
+
+const isStudent = true;
+
+console.log(typeof name);      // Output: string
+console.log(typeof age);       // Output: number
+console.log(typeof isStudent);// Output: boolean
