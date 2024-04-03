5cd7463442ed1eec0de308c3796a795b351c90ab
tatulibozhadze <144163682+tatulibozhadze@users.noreply.github.com>
2024-04-03 10:07:13 -0700

    Add files via upload

---
+++ day56hw.js
@@ -0,0 +1,54 @@
+//შედარებით ოპერატორები 
+console.log(5 == 5); 
+console.log(5 != 6); 
+console.log(10 > 5); 
+console.log(5 < 10); 
+console.log(10 >= 10); 
+console.log(5 <= 5); 
+console.log(5 === 5); 
+console.log(5 !== "5"); 
+console.log(5 < 10 && 10 > 5); 
+console.log(5 < 10 || 10 < 5); 
+//postfix და prefix
+
+let a = 5;
+++x;
+console.log(a); 
+
+let b = 5;
+--x;
+console.log(b); 
+
+let c = 5;
+x++;
+console.log(c); 
+
+let d = 5;
+x--;
+console.log(d); 
+
+
+
+//increment და decrement
+
+let e = 5;
+++x;
+console.log(e); // 6
+
+let f = 5;
+--x;
+console.log(f); // 4
+
+let g = 5;
+x++;
+console.log(g); // 6
+
+let h = 5;
+x--;
+console.log(h); // 4
+
+let i = 5;
+x++;
+console.log(i); // 6
+
+
