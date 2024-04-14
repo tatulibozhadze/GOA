1b49d4aa3fba4455d27c0f5d638d94213e60cf7a
tatulibozhadze <144163682+tatulibozhadze@users.noreply.github.com>
2024-04-14 10:17:26 -0700

    Add files via upload

---
+++ day 60/homework/day60hw.js
@@ -0,0 +1,34 @@
+//first homework
+
+let usersName = prompt("Enter your name and lastname:");
+let momsName = prompt("Enter your moms name and lastname:");
+let dadsName = prompt("Eenter your dads name and lastname:");
+let grandmothersName = prompt("Enter your grandmothers name and lastname:");
+let grandfathersName = prompt("Enter your grandfathers name and lastname:");
+let userAdress = prompt("Enter your adress:");
+
+
+let sentence = `users name: ${usersName}, moms name: ${momsName}, dads name: ${dadsName}, grandmothers name: ${grandmothersName}, grandfathers name: ${dadsName}, adress: ${userAdress}`;
+
+
+console.log(sentence);
+
+//second homework
+
+//cant do it 
+
+//third homework
+
+function square(number) {
+    console.log(number * number);
+  }
+  
+  square(5); 
+  
+//fourth homework
+
+let customerName = prompt("please enter your name")
+let customerAge = prompt("please enter your age")
+
+console.log("Hello," + " " + customerName +" " + "as i remember, you are" + " " +customerAge +" "+ "years old.")
+
