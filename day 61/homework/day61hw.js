ea0cd93860536a3025d58e156627bd5540555def
tatulibozhadze <144163682+tatulibozhadze@users.noreply.github.com>
2024-04-17 11:23:47 -0700

    Add files via upload

---
+++ day 61/homework/day61hw.js
@@ -0,0 +1,46 @@
+// first hw
+
+console.log(5 > 3); 
+console.log(10 < 20); 
+
+let x = 15;
+let y = 20;
+console.log(x < y); 
+
+let a = 100;
+let b = 50;
+console.log(a > b); 
+
+console.log(true > false); 
+console.log(true < 1); 
+
+let sunny = true;
+let rainy = false;
+console.log(sunny > rainy); 
+
+let cold = true;
+let hot = false;
+console.log(cold < hot); 
+
+let number1 = 50
+let number2 = 30
+console.log(number1 < number2)
+
+//second hw
+
+console.log(30 > 15)
+
+console.log(756 > 600)
+
+let tatuli = 5
+let idk = 30
+console.log(tatuli < 30)
+
+let Goa = true
+let otherAcademies = false
+console.log(Goa > otherAcademies)
+
+let GoaW = 34
+let GoaL = 2
+console.log(GoaW > GoaL)
+
