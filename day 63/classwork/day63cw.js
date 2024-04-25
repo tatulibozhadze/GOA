ab76396915b8dc37d378ee2b2d595f26b5d3fc12
tatulibozhadze <144163682+tatulibozhadze@users.noreply.github.com>
2024-04-25 11:03:24 -0700

    Add files via upload

---
+++ day 63/classwork/day63cw.js
@@ -0,0 +1,9 @@
+
+let tatuli = {
+
+name: "tatuli",
+age: 12,
+lastname: "bojadze", 
+address: "idk",
+}
+console.log(`my name is ${tatuli.name} and i am  ${tatuli.age + 1} years old. my lastname is ${tatuli. lastname} and my adress is ${tatuli.address}`)
---
+++ day 63/homework/day63hw.js
@@ -0,0 +1,17 @@
+//1 savarjisho
+
+let user = {
+    name: "tatuli",
+    age: 12,
+    city: "tbilisi"
+};
+
+user.name = "jumberi";
+user.age = 3;
+user.city = "tbilisi";
+
+console.log(user);
+
+
+//2 savarjisho 
+
