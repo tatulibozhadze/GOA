05182654104edb0e827f783ce3032f02af991208
tatulibozhadze <144163682+tatulibozhadze@users.noreply.github.com>
2024-05-01 11:49:07 -0700

    Add files via upload

---
+++ day 65/homework/day63hw.js
@@ -0,0 +1,10 @@
+
+let email = prompt("Enter your email:");
+let password = prompt("Enter your password:");
+
+if (email === "goabest@gmail.com" && password === "goabestt") {
+    console.log("You have permission.");
+} else {
+    console.log("You don't have permission.");
+}
+
